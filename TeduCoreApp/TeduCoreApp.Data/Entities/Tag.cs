ef449cdfb2d47e51ff002aabe05208193c883c68
bvnty1998 <33032612+bvnty1998@users.noreply.github.com>
2020-10-23 23:31:19 +0700

    Bài 11 Triển khai và tạo các enitity
    Triển khai và tạo các enitity để sinh cớ sở dữ liệu

---
+++ TeduCoreApp/TeduCoreApp.Data/Entities/Tag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using TeduCoreApp.Infrastructure.SharedKernel;
+
+namespace TeduCoreApp.Data.Entities
+{
+   public class Tag : DomainEntity<string>
+    {
+        [MaxLength(255)]
+        public string Name { get; set; }
+        [MaxLength(255)]
+        public string Type { get; set; }
+    }
+}
---
+++ TeduCoreApp/TeduCoreApp.Data/Interfaces/IDateTracKing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeduCoreApp.Data.Interfaces
+{
+   public interface IDateTracKing
+    {
+        DateTime DateCreated { set; get; }
+        DateTime DateModified { get; set; }
+    }
+}
